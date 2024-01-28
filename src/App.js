ddc0f26f6f3f9f44ec678db302ce9eda1fe4bd5a
Paras Bankoti <bankotibankoti@gmail.com>
2024-01-28 22:57:22 +0530

    intigrating redux

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import "./App.css";
 import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 import { Feed } from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { Login } from "./Login";
 
 function App() {
+  // pull data from store using selector
+
+  const user = useSelector(selectUser);
+  console.log(user);
   return (
     <div className="App">
       {/* header */}
+
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Left sidebar */}
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Left sidebar */}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/features/userSlice.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+  status: "idle",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload; // payload is object we pass through it
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+//selectors to pull in info from store
+export const selectUser = (state) => state.user.value;
+
+export default userSlice.reducer;
